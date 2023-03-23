66bb20c6374c1ff6f992f3347bd871dd16ab90b6
sheki018 <jasshekinah@gmail.com>
2023-03-23 14:42:53 +0530

    final

--- command/SignUpCommand.java
+++ command/SignUpCommand.java
@@ -13,7 +13,6 @@ public class SignUpCommand extends Command {
 
     @Override
     public void execute(String command) {
-        // user repository register user can be accessible only from signup command - so that multiple instances of user repository cannot be created
         User user = userRepository.getActiveUser();
         if(user != null){
             printer.printError("A user is already signed in. Sign out to proceed. Use 'signout' command.");
